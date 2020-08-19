cba97e9af358c8457d6b1cd7236c5470645644bb
ricky <ricardohyde15@gmail.com>
2020-08-19 07:56:31 -0400

    CountdownData
    - Class to store countdown data

---
+++ app/src/main/java/com/practice/countdown/CountdownData.java
@@ -0,0 +1,120 @@
+package com.practice.countdown;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CountdownData {
+
+    private String id;
+    private String name;
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
+    private int second;
+
+    private long endTime;
+    private Date date;
+
+    //region Accessor Functions
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+    //endregion
+
+    public CountdownData(String id, String name, long endTime) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(endTime);
+
+        this.id = id;
+        this.name = name;
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+        second = c.get(Calendar.SECOND);
+
+        this.date = c.getTime();
+        this.setEndTime(endTime);
+    }
+
+
+}
