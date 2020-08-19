413a0eacc9dc5b75e1c9f59b233f5e39e7bc94da
ricky <ricardohyde15@gmail.com>
2020-08-19 09:01:30 -0400

    9am 2020-08-19 ListView Adapter
    - Created an adapter "CounterWidgetAdapter" to display our countdown timers in our ListView.
    - minor adjustments to timer_list_view.xml

--- app/src/main/java/com/practice/countdown/MainActivity.java
+++ app/src/main/java/com/practice/countdown/MainActivity.java
@@ -10,13 +10,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -26,13 +23,17 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     Button opnEditBtn;
     Button openTimerBtn;
+    CountdownData data[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listView = findViewById(R.id.list_view);
         opnEditBtn = findViewById(R.id.openEdit);
+        openTimerBtn = findViewById(R.id.openTimer);
+
         opnEditBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        openTimerBtn = findViewById(R.id.openTimer);
         openTimerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,18 +49,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
 
-    public static Date getDate(int year, int month, int day) {
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, year);
-        cal.set(Calendar.MONTH, month);
-        cal.set(Calendar.DAY_OF_MONTH, day);
-        cal.set(Calendar.HOUR_OF_DAY, 0);
-        cal.set(Calendar.MINUTE, 0);
-        cal.set(Calendar.SECOND, 0);
-        cal.set(Calendar.MILLISECOND, 0);
-        return cal.getTime();
+        data = new CountdownData[10];
+        CounterWidgetAdapter adapter = new CounterWidgetAdapter(this, data);
+        listView.setAdapter(adapter);
     }
 
     public static Date getTime(int year, int month, int day, int hour, int min, int sec) {
@@ -75,34 +67,25 @@ public class MainActivity extends AppCompatActivity {
         return cal.getTime();
     }
 
-    class MyAdapter extends ArrayAdapter<String> {
+    class CounterWidgetAdapter extends ArrayAdapter<CountdownData> {
 
+        CountdownData data[];
         Context context;
-        String[] titles;
-        String[] descs;
-        int[] images;
 
         MyAdapter (Context context, String[] titles, String[] desc, int[] images) {
             super(context, R.layout.list_element, R.id.title_text, titles);
             this.context = context;
-            this.titles = titles;
-            this.descs = desc;
-            this.images = images;
+            this.data = data;
         }
 
         @NonNull
         @Override
         public View getView(int index, @Nullable View convertView, @NonNull ViewGroup parent) {
             LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View listElement = layoutInflater.inflate(R.layout.list_element, parent, false);
-            ImageView image = listElement.findViewById(R.id.image);
-            TextView title = listElement.findViewById(R.id.title_text);
-            TextView desc = listElement.findViewById(R.id.desc_text);
+            View listElement = layoutInflater.inflate(R.layout.timer_list_view, parent, false);
+            TextView titleTextView = listElement.findViewById(R.id.timer_list_item_title);
+            TextView dateTextView = listElement.findViewById(R.id.timer_list_item_remaining);
 
-            // now we assign the specific values
-            image.setImageResource(images[index]);
-            title.setText(titles[index]);
-            desc.setText(descs[index]);
 
             return listElement;
         }
